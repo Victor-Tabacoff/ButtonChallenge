498e50b5cd02d335247809ea3e14cacc74adaa3a
Victor Tabacoff <victortabacoff2007@gmail.com>
2024-09-05 08:26:35 -0500

    Initial Commit

---
+++ ButtonChallenge/ButtonChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonChallengeApp.swift
+//  ButtonChallenge
+//
+//  Created by Victor B. Tabacoff on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ButtonChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
